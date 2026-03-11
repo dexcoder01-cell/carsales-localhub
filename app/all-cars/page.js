b39d6af838a10582d99978724a7bc638e09e7c99
dexcoder01-cell <dexcoder01@gmail.com>
2026-03-11 18:05:30 +0530

    install scss

---
+++ app/all-cars/page.js
@@ -0,0 +1,24 @@
+import Header from "@/components/Header/navbar";
+// import HeaderMobile from "@/components/Header/mobileNavbar";
+// import Footer from "@/components/Footer/component";
+// import HomeSlider from "@/components/HomeSlider/component";
+// import AuctionCars from "@/components/AuctionCars/component";
+
+// import "./style.scss";
+
+export default function AllCars() {
+  return (
+    <>
+      {/* <HeaderMobile /> */}
+      <Header />
+      <div className="page-container homepage">
+        {/* <HomeSlider /> */}
+        {/* <AuctionCars /> */}
+        <a className="btn view-auction" href="/past-auctions/">
+          View auction results
+        </a>
+      </div>
+      {/* <Footer /> */}
+    </>
+  );
+}
---
+++ components/Header/navbar.js
@@ -0,0 +1,50 @@
+"use client";
+
+import "./style.scss";
+import Image from "next/image";
+import Link from "next/link";
+
+import Search from "./images/search.png";
+
+const Header = () => {
+  return (
+    <header className="main-header">
+      <div className="header-left">
+        <div className="logo">
+          <span className="logo-black">cars</span>
+          <span className="logo-green">&</span>
+          <span className="logo-black">bids</span>
+        </div>
+
+        <nav className="nav-menu">
+          <Link href="/auctions">Auctions</Link>
+          <Link href="/sell" className="sell-btn">Sell a Car</Link>
+          <Link href="/community">Community</Link>
+          <Link href="/what">What’s Cars & Bids?</Link>
+          <Link href="/leaderboard">Leaderboard</Link>
+        </nav>
+      </div>
+
+      <div className="header-right">
+        <div className="search-bar">
+          <Image
+            src={Search}
+            alt="Search"
+            width={20}
+            height={20}
+            className="search-icon"
+          />
+
+          <input
+            type="text"
+            placeholder="Search for cars (ex. BMW, Audi, Ford)"
+          />
+        </div>
+
+        <button className="signup-btn">Sign Up</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
