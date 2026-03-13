12de672407e80d3b580b945d22f50c44c38e01c4
dexcoder01-cell <dexcoder01@gmail.com>
2026-03-13 16:42:47 +0530

    Mostly page is complete

--- components/AuctionCommentsAndBids/component.js
+++ components/AuctionCommentsAndBids/component.js
@@ -1,16 +1,13 @@
-import "./style.scss";
 import Image from "next/image";
+import "./style.scss";
 import Avatar from "./images/avatar.png";
 
 const AuctionCommentsAndBids = () => {
   return (
     <div className="auction-comments-and-bids">
-
-      {/* Comment Field */}
       <div className="comment-field">
         <div className="comment-bar">
           <h3>Comments & Bids</h3>
-
           <div className="dropdown">
             <button>Newest</button>
             <button>Most Upvoted</button>
@@ -18,114 +15,84 @@ const AuctionCommentsAndBids = () => {
             <button>Bid History</button>
           </div>
         </div>
-
         <form>
           <input type="text" placeholder="Add a Comment..." />
         </form>
       </div>
 
-
-      {/* COMMENT ITEM */}
       <div className="comments">
-
         <div className="avatar">
           <span className="user-avatar">
             <Image src={Avatar} alt="User" />
-
-            {/* PLUS ICON */}
-            <svg width="18" height="18" viewBox="0 0 24 24">
-              <path d="M12 5v14M5 12h14" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
-            </svg>
-
+            <i className="fa-solid fa-circle-plus"></i>
           </span>
         </div>
-
-
         <div className="comment-item">
-
           <div className="user-info">
-
             <span className="user-name">Lorem ipsum 1</span>
-
-            {/* VERIFIED ICON */}
-            <svg width="17" height="17" viewBox="0 0 24 24">
-              <path d="M9 12l2 2 4-4" stroke="green" strokeWidth="2" fill="none"/>
+            <svg className="verified" width="17" height="17" fill="none" xmlns="http://www.w3.org/2000/svg" aria-labelledby="v-9QDZkbVMtheikhanv">
+              <title id="v-9QDZkbVMtheikhanv">Registered Bidder</title>
+              <path d="M6.166 1.286c.952-1.715 3.418-1.715 4.37 0l.425.764.84-.24c1.886-.54 3.63 1.205 3.091 3.09l-.24.841.764.425c1.715.952 1.715 3.418 0 4.37l-.764.425.24.84c.54 1.886-1.205 3.63-3.09 3.091l-.841-.24-.424.764c-.953 1.715-3.419 1.715-4.371 0l-.425-.764-.84.24c-1.886.54-3.63-1.205-3.091-3.09l.24-.841-.764-.424c-1.715-.953-1.715-3.419 0-4.371l.764-.425-.24-.84C1.27 3.015 3.015 1.27 4.9 1.81l.841.24.425-.764z" fill="#4AD493"></path>
+              <path d="M11.5 6.351l-3.625 4.5L6 9.033" stroke="#0F2236" strokeLinecap="round" strokeLinejoin="round"></path>
             </svg>
 
-            <span className="rep">
-
-              {/* UP ARROW */}
-              <svg width="14" height="14" viewBox="0 0 24 24">
-                <path d="M12 5l7 7H5z" fill="currentColor"/>
-              </svg>
-
-              4
-            </span>
-
+            <span className="rep"><i className="fa-solid fa-arrow-up"></i> 4</span>
             <span className="bidder">Bidder</span>
             <span className="time">1h</span>
-
           </div>
-
 
           <p className="comment-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
           </p>
 
-
           <div className="interact">
-
-            <button className="btn-upvote">
-
-              <svg width="14" height="14" viewBox="0 0 24 24">
-                <path d="M12 5l7 7H5z" fill="currentColor"/>
-              </svg>
-
-              1
-            </button>
-
-
-            <button className="btn-reply">
-
-              Reply
-
-              <svg width="14" height="14" viewBox="0 0 24 24">
-                <path d="M10 6l-6 6 6 6" stroke="currentColor" strokeWidth="2" fill="none"/>
-              </svg>
-
-            </button>
-
-
-            <button className="btn-flag">
-
-              Flag as inappropriate
-
-              <svg width="14" height="14" viewBox="0 0 24 24">
-                <path d="M6 3v18M6 3h10l-2 4 2 4H6" stroke="currentColor" strokeWidth="2" fill="none"/>
-              </svg>
-
-            </button>
-
+            <button className="btn-upvote"><i className="fa-solid fa-arrow-up"></i> 1 </button>
+            <button className="btn-reply">Reply <i className="fa-solid fa-turn-up"></i></button>
+            <button className="btn-flag">Flag as inappropriate <i className="fa-regular fa-flag"></i></button>
           </div>
-
         </div>
-
       </div>
 
+      <div className="comments">
+        <div className="avatar">
+          <span className="user-avatar">
+            <Image src={Avatar} alt="User" />
+            <i className="fa-solid fa-circle-plus"></i>
+          </span>
+        </div>
+        <div className="comment-item">
+          <div className="user-info">
+            <span className="user-name">Lorem ipsum 2</span>
+            <svg className="verified" width="17" height="17" fill="none" xmlns="http://www.w3.org/2000/svg" aria-labelledby="v-9QDZkbVMtheikhanv">
+              <title id="v-9QDZkbVMtheikhanv">Registered Bidder</title>
+              <path d="M6.166 1.286c.952-1.715 3.418-1.715 4.37 0l.425.764.84-.24c1.886-.54 3.63 1.205 3.091 3.09l-.24.841.764.425c1.715.952 1.715 3.418 0 4.37l-.764.425.24.84c.54 1.886-1.205 3.63-3.09 3.091l-.841-.24-.424.764c-.953 1.715-3.419 1.715-4.371 0l-.425-.764-.84.24c-1.886.54-3.63-1.205-3.091-3.09l.24-.841-.764-.424c-1.715-.953-1.715-3.419 0-4.371l.764-.425-.24-.84C1.27 3.015 3.015 1.27 4.9 1.81l.841.24.425-.764z" fill="#4AD493"></path>
+              <path d="M11.5 6.351l-3.625 4.5L6 9.033" stroke="#0F2236" strokeLinecap="round" strokeLinejoin="round"></path>
+            </svg>
 
-      {/* Similar Cars */}
+            <span className="rep"><i className="fa-solid fa-arrow-up"></i> 87</span>
+            <span className="time">2h</span>
+          </div>
+
+          <p className="comment-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+
+          <div className="interact">
+            <button className="btn-upvote"><i className="fa-solid fa-arrow-up"></i> 1 </button>
+            <button className="btn-reply">Reply <i className="fa-solid fa-turn-up"></i></button>
+            <button className="btn-flag">Flag as inappropriate <i className="fa-regular fa-flag"></i></button>
+          </div>
+        </div>
+      </div>
 
       <div className="similar-cars">
-
         <h6>See also</h6>
-
         <ul>
           <li><a href="#">Manual cars for sale</a></li>
           <li><a href="#">Mazdas for sale</a></li>
           <li><a href="#">Mazda ND Miatas for sale</a></li>
           <li><a href="#">Convertibles for sale</a></li>
         </ul>
-
       </div>
 
     </div>
